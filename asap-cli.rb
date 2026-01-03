5c2ca52120e720555ed78b33dcbdc72526864746
aydinschwa <aydin@recall.ai>
2026-01-02 21:08:46 -0800

    bump version

--- asap-cli.rb
+++ asap-cli.rb
@@ -1,10 +1,10 @@
 class AsapCli < Formula
     desc "CLI tool to deploy static sites to asap-static.site"
     homepage "https://github.com/aydinschwa/asap-cli"
-    url "https://github.com/aydinschwa/asap-cli/releases/download/v1.1.0/asap"
-    sha256 "3b61c0d7842a13c0ca8dcf625ececa2686e3d6ec25cd289606199bf1b90e74ff"
+    url "https://github.com/aydinschwa/asap-cli/releases/download/v1.1.1/asap"
+    sha256 "a5b3d630e0e4ef6f309ccd5fa920153403cf6dd8b8d3a3de5f7af86e7d0228af"
     license "MIT"
-    version "1.1.0"
+    version "1.1.1"
   
     def install
       bin.install "asap"
